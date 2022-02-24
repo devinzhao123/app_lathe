b9e72ba40c41dbeb8e887464e191b61faf33a518
GitHub Action <action@github.com>
2022-02-24 03:23:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/devinzhao123/applathe/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/devinzhao123/applathe/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.devinzhao123.applathe.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.devinzhao123.applathe.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/devinzhao123/applathe/services/MyProjectService.kt
+++ src/main/kotlin/com/github/devinzhao123/applathe/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.devinzhao123.applathe.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.devinzhao123.applathe.MyBundle
 
 class MyProjectService(project: Project) {
 
